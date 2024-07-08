66654359ff0c91e46d97551a0fbc7d7dec0c3ec9
hannah <i_m_fine_100@naver.com>
2024-07-08 17:56:20 +0900

    first commit

---
+++ vue-basic/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <li v-for="(item, index) in sampleArray" :key="item">
+      {{ item }} / {{ index }}
+    </li>
+    <li v-for="(user, index) in otherArray" :key="user.id">
+      {{ user.id }} / {{ user.name }} / {{ index }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sampleArray: ["a", "b", "c", "d"],
+      otherArray: [
+        { id: 0, name: "John" },
+        { id: 1, name: "Kim" },
+        { id: 2, name: "Lee" },
+        { id: 3, name: "Park" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
